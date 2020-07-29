536841fe404db17ec8053856d0d3014e089c333b
Shivam Sharma <shivam231198@gmail.com>
2020-07-29 10:47:25 +0530

    successfully able to set target

---
+++ case/js/script.js
@@ -0,0 +1,78 @@
+function getColor(colors) {
+    clrs = ""
+    colors.forEach(color => {
+        rgb = color.rgb;
+        clrs = clrs + `<div class="c" style="background-color: rgb(${rgb[0]},${rgb[1]},${rgb[2]});"></div>`;
+    });
+    return clrs;
+}
+
+function updateCase(caseID) {
+    const URL = baseURL + "/case"
+
+    $('.loader').addClass('show');
+
+    $.ajax({
+        url: URL,
+        type: 'GET',
+        beforeSend: function(request) {
+            request.setRequestHeader("caseID", caseID);
+        },
+        success: function(data){ 
+            if(data.error){
+                $('.loader').removeClass('show')
+                console.log(data.error);
+            } else {
+                let res_case = data.case;
+                $('.nav .box .case_name').text(res_case.name);
+
+                $('.main .target .sides').empty();
+
+                res_case.target.sides.forEach(side => {
+                    $('.main .target .sides').append(`
+                        <div class="side">
+                            <div class="blck">
+                                <img src="${baseURL + '/images/' + caseID + '/' + side.side + '.jpg'}" alt="">
+                            </div>
+                            <div class="info">
+                                <p class="name">Side : ${side.side}</p>
+                                <p class="feature">Features : error</p>
+                                <p class="clrs">Colors :- </p>
+                                <div class="colors">
+                                    ${getColor(side.colors)}
+                                </div>
+                            </div>
+                        </div>
+                    `)
+                });
+                
+                $('.loader').removeClass('show')
+            }
+        },
+        error: function(err) {
+            $('.loader').removeClass('show')
+            console.log("error" + err);
+        }
+    });
+}
+
+$(document).ready(function(){
+    let caseID = localStorage.getItem('caseID');
+    if(caseID) {
+        updateCase(caseID);
+    } else {
+        console.log('CaseID not found');
+        let url = "/cases";
+        window.location.href = url; 
+    }
+});
+
+$('.nav .box .refresh').click(function() {
+    updateCase();
+});
+
+$('.main .target .add .add_side').click(function() {
+    $('.loader').addClass('show')
+    let url = "/case/settarget";
+    window.location.href = url; 
+});
