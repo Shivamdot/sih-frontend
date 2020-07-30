4273e9ec92494482ea07c70a26b6b835fb61d851
Shivam Sharma <shivam231198@gmail.com>
2020-07-30 18:41:09 +0530

    added analysis process

--- case/js/script.js
+++ case/js/script.js
@@ -58,6 +58,68 @@ function updateCase(caseID) {
     });
 }
 
+let intervalID = 0
+
+function analyse(caseID) {
+    const URL = baseURL + "/gettarget"
+
+    $('.main .analysis .view').removeClass('show')
+    $('.main .analysis .analyse').addClass('hide')
+    $('.main .analysis .load').addClass('show');
+    $('.main .analysis .status p').text('starting the process . . .');
+    $('.main .analysis .status').addClass('show');
+
+    $.ajax({
+        url: URL,
+        type: 'POST',
+        beforeSend: function(request) {
+            request.setRequestHeader("caseID", caseID);
+        },
+        success: function(data){ 
+            if(data.error){
+                $('.main .analysis .load').removeClass('show')
+                $('.main .analysis .status p').text(data.error);
+                $('.main .analysis .analyse').removeClass('hide')
+            } else if(data.success){
+                $('.main .analysis .status p').text("Status : 0%");
+                intervalID = setInterval(getStatus, 5000, caseID);
+            }
+        },
+        error: function(err) {
+            $('.main .analysis .load').removeClass('show')
+            $('.main .analysis .status p').text('Server is down !');
+            $('.main .analysis .analyse').removeClass('hide')
+        }
+    });
+}
+
+function getStatus(caseID) {
+    const URL = baseURL + "/getstatus"
+    $.ajax({
+        url: URL,
+        type: 'GET',
+        beforeSend: function(request) {
+            request.setRequestHeader("caseID", caseID);
+        },
+        success: function(data){ 
+            $('.main .analysis .status p').text(`Status : ${data.status}%`);
+            if(data.status == -1 || data.status == 100){
+                clearInterval(intervalID);
+                $('.main .analysis .load').removeClass('show')
+                $('.main .analysis .status p').text("processing complete.");
+                $('.main .analysis .analyse').removeClass('hide')
+                $('.main .analysis .view').addClass('show')
+            }
+        },
+        error: function(err) {
+            clearInterval(intervalID);
+            $('.main .analysis .load').removeClass('show')
+            $('.main .analysis .status p').text('Server is down !');
+            $('.main .analysis .analyse').removeClass('hide')
+        }
+    });
+}
+
 $(document).ready(function(){
     let caseID = localStorage.getItem('caseID');
     if(caseID) {
@@ -76,5 +138,23 @@ $('.nav .box .refresh').click(function() {
 $('.main .target .add .add_side').click(function() {
     $('.loader').addClass('show')
     let url = "/case/settarget";
+    window.location.href = url; 
+});
+
+$('.main .analysis .analyse').click(function() {
+    let caseID = localStorage.getItem('caseID');
+    if(caseID) {
+        analyse(caseID);
+    } else {
+        console.log('CaseID not found');
+        let url = "/cases";
+        window.location.href = url; 
+    }
+});
+
+
+$('.main .analysis .view').click(function() {
+    $('.loader').addClass('show')
+    let url = "/case/analysis";
     window.location.href = url; 
 });
